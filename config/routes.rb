eaf30de50e426135f94b7e0fc6377ea1ae46b595
kai <kai@newlionlabs.com>
2017-01-26 06:57:53 +0000

    askljf

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :notes
-  get '/' => 'notes#index'
+  get '/', to: 'home#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125182246) do
+ActiveRecord::Schema.define(version: 20170126050627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
