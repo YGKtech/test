e35c3e73ce53bc3e32a1b0b37f64ea53ea067bf7
kai <kai@newlionlabs.com>
2017-03-01 23:41:55 +0000

    asd

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,8 @@
 module HomeHelper
+  def url2
+    'promere://connect#reset_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXNldFN0cmluZyI6ImE5MzMzOTVjMjliYjYyNDMiLCJpYXQiOjE0ODg0MDgwMjQsImV4cCI6MTQ4ODQ5NDQyNH0.Ubb7DL7vWaDU_z1yPHiMykLPkNuPTyUhphAhgov3UMk&expiry=2017-03-02T08%3A00%3A00.000Z&server_url=https%3A%2F%2Fscotus2.promere.io&company_name=Supreme%20Court%20of%20the%20USA&email_address=kai%40newlionlabs.com'
+  end
+  def url3
+    'promere://connect?reset_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXNldFN0cmluZyI6ImE5MzMzOTVjMjliYjYyNDMiLCJpYXQiOjE0ODg0MDgwMjQsImV4cCI6MTQ4ODQ5NDQyNH0.Ubb7DL7vWaDU_z1yPHiMykLPkNuPTyUhphAhgov3UMk&expiry=2017-03-02T08%3A00%3A00.000Z&server_url=https%3A%2F%2Fscotus2.promere.io&company_name=Supreme%20Court%20of%20the%20USA&email_address=kai%40newlionlabs.com'
+  end
 end
